ab6e0808c362b2a3b82b8fbce0ebd9af7772cb79
jnomded <james.edmond04@gmail.com>
2025-12-07 10:48:42 -0600

    Sheep are white, or red if malnourished, take random steps in any orthogonal direction

--- sheep_sim.py
+++ sheep_sim.py
@@ -6,3 +6,22 @@ import os
 from config_loader import get_all_params
 from sheep_model import step_eueler, functional_response
 
+# Sheep status global
+
+COLOR_SHEEP_OK = np.array([255, 255, 255])
+COLOR_SHEEP_BAD = np.array([255, 0, 0])
+COLOR_TEXT = (255, 255, 255)
+
+class Sheep:
+    """Visual representation of a sheep on the grid."""
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, grid_size):
+        """Random walk movement."""
+        moves = [(0, 1), (0, -1), (1, 0), (-1, 0), (0, 0)]
+        dx, dy = random.choice(moves)
+        self.x = max(0, min(grid_size - 1, self.x + dx))
+        self.y = max(0, min(grid_size - 1, self.y + dy))
